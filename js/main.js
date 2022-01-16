74ce592c40ba46aa109dc95409e521831e7e67da
Zaki-Dz <berkoukes2015@gmail.com>
2022-01-16 17:41:30 +0100

    hamburger menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,35 @@
-$(document).ready(function(){
+let menuBtn = document.querySelector('.menu-btn')
 
-	
+let menu = document.querySelector('header nav ul')
 
-});
+let ind = false
+
+menuBtn.addEventListener('click', () => {
+
+	if (!ind) {
+
+		menu.style.left = 0
+		menuBtn.style.color = '#178573'
+		ind = true
+
+	} else {
+
+		menu.style.left = '100%'
+		menuBtn.style.color = '#1FB299'
+		ind = false
+
+	}
+
+})
+
+window.addEventListener('click', (el) => {
+
+	if (!el.target.classList.contains('menu-btn') && ind) {
+
+		menu.style.left = '100%'
+		menuBtn.style.color = '#1FB299'
+		ind = false
+
+	}
+
+})
